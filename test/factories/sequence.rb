6bfa2792de4715d71aec45f52edb5be1cc610721
Aleksandr Pushin <pushinap@>
2021-01-15 20:56:36 +0300

    Add relations and validatons for models Task and User

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   sequence :string, aliases: [:first_name, :last_name, :password] do |n|
     "string#{n}"
   end
+
+  sequence :email do |n|
+    "user#{n}@test.ru"
+  end  
 end
