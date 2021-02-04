d8ef90bdc5cbafcb65ef487c6f7838d0303e8034
Aleksandr Pushin <pushinap@>
2021-02-05 01:21:18 +0300

    Rubocop inspected

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class Web::SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
-
 end
