a7edec1ad3fe0349549d6b57f056dd203971dfcb
Aleksandr Pushin <pushinap@>
2021-02-05 20:16:17 +0300

    Add tests for Session controller

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -5,4 +5,21 @@ class Web::SessionsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "should post create" do
+    user = create(:user)
+    
+    attrs = {
+      email: user.email,
+      password: user.password
+    }
+    
+    post :create, params: { session_form: attrs }
+    assert_response :redirect
+  end
+
+  test "should delete destroy" do
+    delete :destroy
+    assert_response :redirect
+  end
 end
