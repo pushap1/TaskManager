b2937430c256ce2e9bae59e27f08166851c7e27c
pushap <31646089+PushinAP@users.noreply.github.com>
2021-02-19 11:12:12 +0300

    Add component TaskBoard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,10 @@ import 'material-design-lite/material.js';
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from '../components/TaskBoard'
 
+WebpackerReact.setup({ TaskBoard })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
