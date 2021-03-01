1edf2c5e960770ad4f850c8e1d55159d9c3bc435
pushap <31646089+PushinAP@users.noreply.github.com>
2021-03-01 18:25:21 +0300

    Change status

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -58,6 +58,22 @@ const TaskBoard = () => {
     });
   };
 
+  const handleCardDragEnd = (task, source, destination) => {
+    const transition = task.transitions.find(({ to }) => destination.toColumnId === to);
+    if (!transition) {
+      return null;
+    }
+
+    return TasksRepository.update(task.id, { task: { stateEvent: transition.event } })
+      .then(() => {
+        loadColumnInitial(destination.toColumnId);
+        loadColumnInitial(source.fromColumnId);
+      })
+      .catch((error) => {
+        alert(`Move failed! ${error.message}`);
+      });
+  };
+
   const generateBoard = () => {
     const newBoard = {
       columns: STATES.map(({ key, value }) => ({
@@ -81,6 +97,7 @@ const TaskBoard = () => {
     <KanbanBoard
       renderCard={(card) => <Task task={card} />}
       renderColumnHeader={(column) => <ColumnHeader column={column} onLoadMore={loadColumnMore} />}
+      onCardDragEnd={handleCardDragEnd}
     >
       {board}
     </KanbanBoard>
