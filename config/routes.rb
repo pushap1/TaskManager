3551a5114491cd444e9b0aa585468e3e587571cf
pushap <31646089+PushinAP@users.noreply.github.com>
2021-03-05 00:14:13 +0300

    Add resourse users to routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy], defaults: { format: 'json' }
+      resources :users, only: [:index, :show], defaults: {format: 'json'}
     end
   end
 end
