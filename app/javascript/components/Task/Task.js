ae84e2a13b55b70ab400a04ba5286371dd386b6c
pushap <31646089+PushinAP@users.noreply.github.com>
2021-03-04 22:04:13 +0300

    Add actions Edit and Delete to component Task

--- app/javascript/components/Task/Task.js
+++ app/javascript/components/Task/Task.js
@@ -5,15 +5,36 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CardActions from '@material-ui/core/CardActions';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import useStyles from './useStyles';
 
-const Task = ({ task }) => {
+const Task = ({ task, onOpenEditPopup, onDeleteTask }) => {
   const styles = useStyles();
+
+  const handleEdit = () => onOpenEditPopup(task);
+  const handleDelete = () =>
+    onDeleteTask(task).catch((error) => {
+      alert(`Destrucion Failed! Error: ${error.message}`);
+    });
+
+  const action = (
+    <CardActions>
+      <IconButton onClick={handleDelete}>
+        <DeleteIcon />
+      </IconButton>
+      <IconButton onClick={handleEdit}>
+        <EditIcon />
+      </IconButton>
+    </CardActions>
+  );
 
   return (
     <Card className={styles.root}>
-      <CardHeader title={task.name} />
+      <CardHeader action={action} title={task.name} />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {task.description}
@@ -25,6 +46,8 @@ const Task = ({ task }) => {
 
 Task.propTypes = {
   task: PropTypes.shape().isRequired,
+  onOpenEditPopup: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
